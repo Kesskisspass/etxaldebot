39503b50903e34e73523acc511197651956efc0d
kesskisspass <kesskisspass@gmail.com>
2020-05-08 08:46:13 +0200

    add comments and cleaning import localisation db

--- db/localisations.py
+++ db/localisations.py
@@ -3,33 +3,46 @@
 import csv
 import pymysql
 
-
 db = pymysql.connect("localhost","root","root","etxaldebot" )
 cursor = db.cursor()
 
+# Création de la table
+cursor.execute("DROP TABLE IF EXISTS localisations")
+
+sql_table = """CREATE TABLE localisations (
+   ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   NOM  CHAR(50) NOT NULL,
+   NOM_SLUG  CHAR(50) NOT NULL,
+   CODE_POSTAL  CHAR(6),
+   LONGITUDE CHAR(30),  
+   LATITUDE CHAR(30) )"""
+cursor.execute(sql_table)
+
+# Import des lignes en parcourant le csv
 with open('localisation/communes_64.csv', mode='r') as csv_file:
     fieldnames = ['nom_commune','slug','codepostal','latitude','longitude']
     csv_reader = csv.DictReader(csv_file, fieldnames=fieldnames)
     flag = False
     for row in csv_reader:
 
-        sql = "INSERT INTO localisations(NOM, \
+        sql_rows = "INSERT INTO localisations(NOM, \
         NOM_SLUG, CODE_POSTAL, LONGITUDE, LATITUDE) \
         VALUES ('%s', '%s', '%s', '%s', '%s' )" % \
         (row['nom_commune'], row['slug'], row['codepostal'], row['latitude'],row['longitude'])
         
-        # print(row['nom_commune'], row['slug'], row['codepostal'], row['latitude'],row['longitude'])
-        
         try:
-        # Execute the SQL command
-            cursor.execute(sql)
-        # Commit your changes in the database
+            cursor.execute(sql_rows)
             db.commit()
         except:
-        # Rollback in case there is any error
+
             print("Erreur", row['nom_commune'])
             flag = True
             db.rollback()
+
+# deconnection mysql
+db.close()
+
+# Feedback console sur l'import
 if (flag):
     print("Erreur à l'import")
 else:
