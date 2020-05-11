edbeca90df1601c75bcf40b09fc637f7b262afd8
kesskisspass <kesskisspass@gmail.com>
2020-05-11 16:12:59 +0200

    fix création table cat_gen

--- db/cat_gen.py
+++ db/cat_gen.py
@@ -1,6 +1,5 @@
-# Import des catégories produits avec colonne bio (1/0)
-import re
-import csv
+# Création de la table cat_generale
+
 import pymysql
 
 db = pymysql.connect("localhost","root","root","etxaldebot" )
@@ -11,29 +10,17 @@ cursor.execute("DROP TABLE IF EXISTS cat_generale")
 
 sql_table = """CREATE TABLE cat_generale (
    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   nom  CHAR(100) NOT NULL
+    nom CHAR(100) NOT NULL
    )"""
 cursor.execute(sql_table)
 
-# Import des lignes en parcourant le csv
-with open('scrap/categ_prod.csv', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    flag = False
-    for row in csv_reader:
-        test = row[0].lower().strip()
-        #print(test)
-        try:
-            if(re.search(r'.*fromages?.*|.*lait.*',test).group()):
-                sql_rows = "INSERT INTO cat_generale(nom) \
-                VALUES ('fromage et produits laitier' )"
-        
-                try:
-                    cursor.execute(sql_rows)
-                    db.commit()
-                except:
+liste = ['fromages et produits laitiers','fruits et légumes','viandes','boissons','autres']
 
-                    print("Erreur", cat)
-                    flag = True
-                    db.rollback()
-        except:
-            pass
+sql_rows = "INSERT INTO cat_generale(nom)  VALUES ('%s')"          
+try:
+    cursor.executemany(sql_rows,liste)
+    db.commit()
+except:
+    print("Erreur", liste)
+    db.rollback()
+db.close()
